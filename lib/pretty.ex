98675819bf4fbd0beda2422c7373ee4fd1ab61bb
Andreas Tell <git@tell.nu>
2022-12-17 10:02:42 +0100

    add Pretty.grid/2

--- lib/pretty.ex
+++ lib/pretty.ex
@@ -6,7 +6,16 @@ defmodule Pretty do
   @doc ~S"""
   Converts `term` to a pretty canvas.
   """
+  @spec from(term) :: Pretty.Canvas.t()
   def from(term) do
     Pretty.From.term(term)
+  end
+
+  @doc ~S"""
+  Returns a pretty canvas with a grid.
+  """
+  @spec grid([term], Keyword.t()) :: Pretty.Canvas.t()
+  def grid(list, options \\ []) do
+    Pretty.From.list(list) |> Pretty.Compose.grid(options)
   end
 end
